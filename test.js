762f73a131cf3b59fa1632e574a23554214c46ba
Perseusxe <perseus20061103@gmail.com>
2024-11-01 18:12:44 +0800

    friday bodlogo

---
+++ test.js
@@ -0,0 +1,195 @@
+// let y=2100;
+// if(y % 4 === 0 && y % 100 !=0 || y % 400 === 0){
+//    console.log("high")
+// }else {
+//     console.log("low")
+// }
+
+
+
+
+// let a=20;
+// let b=10;
+// if(a>b){
+//     console.log("A")
+// }else {
+//     console.log("B")
+// }
+
+
+
+// let c=30;
+// if(a>b && a>c){
+//     console.log('A')
+// }else if(b>a && b>c){
+//     console.log("B")
+// }else if(c>a && c>b){
+//     console.log("C")
+// }
+
+
+// if(a>b && b>c){
+//     console.log("A>B>C")
+// }else if(b>a && c>a){
+//     console.log("B>C>A")
+// }else if(b>a && a>c){
+//     console.log("B>A>C")
+// }else if(c>a && a>b){
+//     console.log("C>A>B")
+// }
+
+
+// 10 удаа  "Hello!" гэж хэвлэх
+// let word="Hello!";
+// let count=0;
+
+// while(count<10) {
+//     count = count + 1;
+//     console.log(word)
+// } 
+
+
+// 1-ээс 10 хүртэлх тоог хэвлэх
+// let number=0;
+
+// while(number<10) {
+//     number=number+1;
+//     console.log(number)
+// }
+
+
+// 1-ээс 20 хүртэлх тооны нийлбэр олох
+// let n=1;
+// sum=0;
+
+// while(n<=21) {
+//     console.log(sum)
+//     sum=sum+n, 
+//     n=n+1
+// }
+
+
+// 1-ээс 20 хүртэлх сондгой тоонуудыг хэвлэх
+// while(n<=20) {
+//     if(   n%2==1  ) {console.log(n)} 
+//     n=n+1 
+// }
+// let n=1;
+// let a=1;
+
+
+
+// Дурын тоо өгөгдөхөд тухайн тооны факториал олох
+// while(a=n) {
+//    a=a*n;
+//    n=n++;
+//     console.log(a)
+// }
+
+
+
+// Тооны хүрд харуулах програм бич.
+// let w=1,
+// r=3;
+// while(w<10) {
+// z=r*w;
+// w=w+1
+// console.log(w, "x", r, "=", z+r)
+// }
+
+
+// //Дурын тоо өгөгдөхөд  тухайн тооны урвууг хэвлэ. Зөвхөн эерэг то байна.
+// let a=1,
+// b=2,
+// c=3;
+// while(z>0) {
+//   m= a , b , c;
+//   z= c , b , a;
+//   console.log(m , z)
+// }
+
+//Budget Tracker
+// let budget=100,
+// days=1,
+// expense=0;
+// while(expense<=budget){
+// if(days<=5){
+//     expense=expense+5;
+// }else if(days>=5 && days<=7){
+//     expense=expense+10;
+// }
+// console.log(days, expense)
+// if(expense>=80 && expense<=budget){
+//     console.log("Таны нийт зардал 100$ руу ойртож байна шүү")
+// }
+// if(expense<=budget){
+//     console.log("Та зардлын хязгаарт хүрлээ!")
+// }
+
+// if(days>6){
+//     days=1;
+// }else{
+//     days++
+// }
+// }
+
+
+//Coffee shop punch card
+// let coffee=0;
+// while(coffee<=9){
+//     if(coffee=coffee+1){
+//         console.log("Та", coffee, "дахь кофегоо худалдан авлаа")
+//     }
+//     if(coffee % 3 ==0){
+//         console.log("Танд үнэгүй бялуу бэлэглжу байна")
+//     }
+//     if(coffee % 10 ==0){
+//         console.log("Баяр хүргэе! Та үнэгүй кофе авлаа!")
+//     }
+// } console.log("Урамшууллын карт дууслаа!")
+
+// let a=1,
+// b=2,
+// c=3,
+// d=4;
+// if(a>=b && b>=c && b>=d) {
+//     console.log(b)
+// }else if(a>=c && c>=b && c>=d){
+//     console.log(c)
+// }else if(a>=d && d>=b && d>=c){
+//     console.log(d)
+// }else if(b>=a && a>=c && a>=d){
+//     console.log(a)
+// }else if(b>=c && c>=a && c>=d){
+//     console.log(c)
+// }else if(b>=d && d>=a && d>=c){
+//     console.log(d)
+// }else if(c>=a && a>=b && a>=d){
+//     console.log(a)
+// }else if(c>=d && d>=a && d>=b){
+//     console.log(d)
+// }else if(c>=b && b>=a && b>=d){
+//     console.log(b)
+// }else if(d>=a && a>=b && a>=c){
+//     console.log(a)
+// }else if(d>=b && b>=a && b>=c){
+//     console.log(b)
+// }else if(d>=c && c>=a && c>=b){
+//     console.log(c)
+// }
+
+// let number=3,
+// third=0
+// sum=0
+
+// while(number<1000){
+//     number=number+3;
+//     sum=number+sum;
+//     console.log(sum)
+// }
+let i=0;
+var star="*"
+while(i<=7){
+    i=i+1;
+    console.log(star.repeat(i))
+}
