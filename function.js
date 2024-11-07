7945cd072d999a8e3afd1e9e06c89db1d8ea4d2b
Perseusxe <perseus20061103@gmail.com>
2024-11-07 18:08:05 +0800

    todo

--- function.js
+++ function.js
@@ -62,45 +62,45 @@
 // }
 // findDigitCount(2222, 2)
 
-const books=[
-    {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    year: 1960
-},
-{
-    title: "1984",
-    author: "George Orwell",
-    year: 1949
-},
-{
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: 1925
-},
-{
-    title: "One Hundred Years of Solitude",
-    author: "Gabriel Garcia Marquez",
-    year: 1967
-},
-{
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    year: 1813
-}
-] 
+// const books=[
+//     {
+//     title: "To Kill a Mockingbird",
+//     author: "Harper Lee",
+//     year: 1960
+// },
+// {
+//     title: "1984",
+//     author: "George Orwell",
+//     year: 1949
+// },
+// {
+//     title: "The Great Gatsby",
+//     author: "F. Scott Fitzgerald",
+//     year: 1925
+// },
+// {
+//     title: "One Hundred Years of Solitude",
+//     author: "Gabriel Garcia Marquez",
+//     year: 1967
+// },
+// {
+//     title: "Pride and Prejudice",
+//     author: "Jane Austen",
+//     year: 1813
+// }
+// ] 
 
 
 
-function filterBooksByYear(  filterYear){
-    let arr = []
-    for( let i=0;  i< books.length; i++){
-        if( books[i].year  >= filterYear){
-            arr.push( books[i])
-        }
-    }
-    return arr
-}
+// function filterBooksByYear(  filterYear){
+//     let arr = []
+//     for( let i=0;  i< books.length; i++){
+//         if( books[i].year  >= filterYear){
+//             arr.push( books[i])
+//         }
+//     }
+//     return arr
+// }
 
 
-console.log(filterBooksByYear(1950))
+// console.log(filterBooksByYear(1950))
---
+++ todo.js
@@ -0,0 +1,44 @@
+let todos = [];
+
+// Todo add
+function addOne(newTodo) {
+  todos.push(newTodo);
+}
+
+// Change Status : Func
+function editStatus(index, status) {
+  let item = todos[index];
+  item.status = status;
+}
+
+// Change Name : Func
+function editName(index, name) {
+  let item = todos[index];
+  item.name = name;
+}
+
+// Delete one item
+function deleteOne(index) {
+  let temp = [];
+
+  for (let i = 0; i<todos.length; i++) {
+if(i != index) {
+    temp.push(todos[i]);
+}
+  }
+  todos = temp;
+}
+
+// Delete All
+function deleteAll() {
+    todos = [];
+}
+
+// Running Application
+addOne({ name: "Gym", status: "TODO" });
+addOne({ name: "Learn JS", status: "TODO" });
+addOne({ name: "Read a Book", status: "TODO" });
+editStatus(2, "DONE");
+editName(0, "Gym");
+deleteOne();
+console.log(todos);
